d12b137419797554a318186a899f6583b554f641
Marco Juergens <juergens@bbaw.de>
2012-05-31 11:24:07 +0000

    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@132 4cd7446e-361a-11e1-ae49-e5e889259ed6

---
+++ src/org/bbaw/wsp/cms/translator/MicrosoftTranslator.java
@@ -0,0 +1,99 @@
+package org.bbaw.wsp.cms.translator;
+
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
+import com.memetix.mst.detect.Detect;
+
+
+public class MicrosoftTranslator {
+	
+	private static final String key = "474A4E72DB217E37031EC190ACB4159378A6917C";
+	
+	//prevent instantiation
+	private MicrosoftTranslator(){}
+	
+	
+	public static String translateQuery(String queryToTranslate) throws Exception {
+        // Set the API key once per JVM. It is set statically and applies to all services
+        Translate.setKey(key);
+        
+     // From AUTO_DETECT -> German 
+        System.out.println("queryToTranslate : "+queryToTranslate);
+        String translatedText = Translate.execute(queryToTranslate, Language.GERMAN);
+        System.out.println("queryToTranslate AUTO_DETECT -> German : " + translatedText);
+        
+        
+        // From AUTO_DETECT -> French 
+//        translatedText = Translate.execute("Bonjour", Language.FRENCH, Language.ENGLISH);
+        String ranslatedText = Translate.execute(queryToTranslate, Language.FRENCH);
+        System.out.println("queryToTranslate AUTO_DETECT -> French : " + ranslatedText);
+       
+        // English AUTO_DETECT -> Arabic
+        String anslatedText = Translate.execute(queryToTranslate, Language.ARABIC);
+        System.out.println("queryToTranslate AUTO_DETECT -> Arabic: " + anslatedText);
+        
+        return translatedText;
+    }
+
+	public static String[] translateQuery(String[] queryToTranslate) throws Exception {
+        // Set the API key once per JVM. It is set statically and applies to all services
+        Translate.setKey(key);
+        
+     // From AUTO_DETECT -> German 
+        System.out.println("queryToTranslate : "+queryToTranslate);
+        String[] translatedText = Translate.execute(queryToTranslate, Language.GERMAN);
+        System.out.println("queryToTranslate AUTO_DETECT -> German : ");
+        for (String string : translatedText) {
+        	System.out.print(string);
+        	System.out.print(" ");
+		}
+        System.out.println();
+        
+        
+        // From AUTO_DETECT -> French 
+//        translatedText = Translate.execute("Bonjour", Language.FRENCH, Language.ENGLISH);
+        String[] ranslatedText = Translate.execute(queryToTranslate, Language.FRENCH);
+        System.out.println("queryToTranslate AUTO_DETECT -> French : ");
+        for (String string : ranslatedText) {
+        	System.out.print(string);
+        	System.out.print(" ");
+		}
+        System.out.println();
+       
+        // English AUTO_DETECT -> Arabic
+        String[] anslatedText = Translate.execute(queryToTranslate, Language.ARABIC);
+        System.out.println("queryToTranslate AUTO_DETECT -> Arabic : ");
+        for (String string : anslatedText) {
+        	System.out.print(string);
+        	System.out.print(" ");
+		}
+        System.out.println();
+        return translatedText;
+    }
+	
+	public String detectLanguageCode(String queryToDetect) throws Exception{
+	
+		//Detect returns a Language Enum representing the language code
+	    Language detectedLanguageCode = Detect.execute(queryToDetect);
+	    
+	    return detectedLanguageCode.toString();
+	}
+	 
+	public String detectLanguageName(String queryToDetect) throws Exception{
+		
+		Language detectedLanguageCode = Detect.execute(queryToDetect);
+		
+		//Examples:
+	    //Prints the Language name in English - "French"
+	    String detectedLanguageName = detectedLanguageCode.getName(Language.ENGLISH);
+	    
+	    //Prints the Language name in French - "Français"
+	    System.out.println(detectedLanguageCode.getName(Language.FRENCH));
+	    
+	    //Prints the Language name in German - "Französisch"
+	    System.out.println(detectedLanguageCode.getName(Language.GERMAN));
+	    
+	    return detectedLanguageName;
+	}
+	
+}
