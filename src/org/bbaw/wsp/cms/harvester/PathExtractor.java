1e90c485baf0b812589eeca81ccf58eb9c6549c1
Marco Juergens <juergens@bbaw.de>
2012-06-15 14:58:18 +0000

    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@243 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/harvester/PathExtractor.java
+++ src/org/bbaw/wsp/cms/harvester/PathExtractor.java
@@ -19,134 +19,119 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 public class PathExtractor {
 
-	private List<String> ressourceLoc;
-//	private List<BbawDocument> parsingResultsList;
-	PrintWriter out;
-	
-	public PathExtractor(){
-		
-	}
-	
-//	public PathExtractor(PrintWriter out){
-//		System.out = out;
-//	}
-	
-	public List<String> initExtractor(String startingUri){
-		
-        ressourceLoc = new ArrayList<String>(); 
-        
-        //parameter necessery, because it's recursive, thus changing the uri
-       	extractDocLocations(startingUri);
+  private List<String> ressourceLoc;
+  PrintWriter out;
 
-		System.out.println("<br>");
-		System.out.println("extracing resource locations done.");
-		System.out.println("<br>");
-//	 	for (String s : ressourceLoc) {
-//			System.out.println("resource Locations : "+s);
-//		}
-		return this.ressourceLoc;
-	}
-	
-	/**
-	 * recursive Method to extract the path of the resources
-	 * @param startUrl
-	 */
-	private void extractDocLocations(String startUrl) {
-        HttpClient client = new DefaultHttpClient();
-       	HttpGet httpget = new HttpGet(startUrl);
-		HttpResponse resp = null;
-		try {
-			resp = client.execute(httpget);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-       	HttpEntity entity = resp.getEntity();
-       	if (entity != null) {
-       		XMLInputFactory iFactory = XMLInputFactory.newInstance();
-       		XMLStreamReader reader = null;
-				try {
-					reader = iFactory.createXMLStreamReader(entity.getContent());
-				} catch (IllegalStateException e1) {
-					e1.printStackTrace();
-				} catch (XMLStreamException e1) {
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+  public PathExtractor() {
 
-       		try {
-				while(true){
-					int event = reader.next();
-				    if (event == XMLStreamConstants.END_DOCUMENT) {
-				    	reader.close();
-				       break;
-				    }
-				    if (event == XMLStreamConstants.START_ELEMENT) {
-				    	if((reader.getAttributeValue(null, "name")) != null){
-				    		if(!(reader.getAttributeValue(null, "name")).contains(".") && !(startUrl.endsWith(reader.getAttributeValue(null, "name")))){
-					    		if(reader.getAttributeValue(null, "name").startsWith("/")){
-					    			client.getConnectionManager().closeExpiredConnections();
-					    			extractDocLocations(startUrl + reader.getAttributeValue(null, "name"));
-					    		}else{
-					    			client.getConnectionManager().closeExpiredConnections();
-					    			if(!startUrl.endsWith("/")){
-					    				extractDocLocations(startUrl + "/" + reader.getAttributeValue(null, "name"));
-					    			}else{
-					    				extractDocLocations(startUrl+ reader.getAttributeValue(null, "name"));
-					    			}
-					    		}
-				    		}
-				    		if(reader.getAttributeValue(null, "name").endsWith(".xml")){
-				    			if(!startUrl.endsWith("/")){
-				    				ressourceLoc.add(startUrl + "/" + reader.getAttributeValue(null, "name"));
-				    			}else{
-				    				ressourceLoc.add(startUrl + reader.getAttributeValue(null, "name"));
-				    			}
-					    	}
-				    	}		
-				    }
-				    if(event == XMLStreamConstants.ATTRIBUTE){
-//				    	System.out.println("tss "+reader.getLocalName());
-				    }
-				}
-			} catch (XMLStreamException e) {
-				e.printStackTrace();
-			}
-		}	
-	}
-	
-	/**
-	 * extrahiert ebenso wie extractDocLocations(String startUri) Pfade, tut dies aber local und nicht über HTTP
-	 * @return
-	 */
-	public List<String> extractPathLocally(String startUrl){
-		List<String> pathList = new ArrayList<String>();
-		
-		//home verzeichnis pfad über system variable
-//		String loc = System.getenv("HOME")+"/wsp/configs";
-//		out.println("hom variable + conf datei : "+loc);
-		File f = new File(startUrl);
-//		out.println("readable : "+Boolean.toString(f.canRead()));
-//		out.println("readable : "+f.isDirectory());
-		if(f.isDirectory()){
-			File[] filelist = f.listFiles();
-			for (File file : filelist) {
-				if(file.getName().toLowerCase().contains("config")){
-					if(!startUrl.endsWith("/")){
-						pathList.add(startUrl+"/"+file.getName());
-					}else{
-						pathList.add(startUrl+file.getName());
-					}
-				}
-			}
-		}
-//		
-//		for (String string : pathList) {
-//
-//			out.println("in configslist : "+string);
-//			out.println("<br/>");
-//		}
-		return pathList;
-	}
-	
+  }
+
+  public List<String> initExtractor(String startingUri) {
+
+    ressourceLoc = new ArrayList<String>();
+    // parameter necessery, because it's recursive, thus changing the uri
+    extractDocLocations(startingUri);
+    System.out.println("extracing resource locations done.");
+    return this.ressourceLoc;
+  }
+
+  /**
+   * recursive Method to extract the path of the resources
+   * 
+   * @param startUrl
+   */
+  private void extractDocLocations(String startUrl) {
+    HttpClient client = new DefaultHttpClient();
+    HttpGet httpget = new HttpGet(startUrl);
+    HttpResponse resp = null;
+    try {
+      resp = client.execute(httpget);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    HttpEntity entity = resp.getEntity();
+    if (entity != null) {
+      XMLInputFactory iFactory = XMLInputFactory.newInstance();
+      XMLStreamReader reader = null;
+      try {
+        reader = iFactory.createXMLStreamReader(entity.getContent());
+      } catch (IllegalStateException e1) {
+        e1.printStackTrace();
+      } catch (XMLStreamException e1) {
+        e1.printStackTrace();
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+
+      try {
+        while (true) {
+          int event = reader.next();
+          if (event == XMLStreamConstants.END_DOCUMENT) {
+            reader.close();
+            break;
+          }
+          if (event == XMLStreamConstants.START_ELEMENT) {
+            if ((reader.getAttributeValue(null, "name")) != null) {
+              if (!(reader.getAttributeValue(null, "name")).contains(".") && !(startUrl.endsWith(reader.getAttributeValue(null, "name")))) {
+                if (reader.getAttributeValue(null, "name").startsWith("/")) {
+                  client.getConnectionManager().closeExpiredConnections();
+                  extractDocLocations(startUrl + reader.getAttributeValue(null, "name"));
+                } else {
+                  client.getConnectionManager().closeExpiredConnections();
+                  if (!startUrl.endsWith("/")) {
+                    extractDocLocations(startUrl + "/" + reader.getAttributeValue(null, "name"));
+                  } else {
+                    extractDocLocations(startUrl + reader.getAttributeValue(null, "name"));
+                  }
+                }
+              }
+              if (reader.getAttributeValue(null, "name").endsWith(".xml")) {
+                if (!startUrl.endsWith("/")) {
+                  ressourceLoc.add(startUrl + "/" + reader.getAttributeValue(null, "name"));
+                } else {
+                  ressourceLoc.add(startUrl + reader.getAttributeValue(null, "name"));
+                }
+              }
+            }
+          }
+          if (event == XMLStreamConstants.ATTRIBUTE) {
+            // System.out.println("tss "+reader.getLocalName());
+          }
+        }
+      } catch (XMLStreamException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  /**
+   * extrahiert ebenso wie extractDocLocations(String startUri) Pfade, tut dies
+   * aber local und nicht über HTTP
+   * 
+   * @return
+   */
+  public List<String> extractPathLocally(String startUrl) {
+    List<String> pathList = new ArrayList<String>();
+
+    // home verzeichnis pfad über system variable
+    // String loc = System.getenv("HOME")+"/wsp/configs";
+    // out.println("hom variable + conf datei : "+loc);
+    File f = new File(startUrl);
+    // out.println("readable : "+Boolean.toString(f.canRead()));
+    // out.println("readable : "+f.isDirectory());
+    if (f.isDirectory()) {
+      File[] filelist = f.listFiles();
+      for (File file : filelist) {
+        if (file.getName().toLowerCase().contains("config")) {
+          if (!startUrl.endsWith("/")) {
+            pathList.add(startUrl + "/" + file.getName());
+          } else {
+            pathList.add(startUrl + file.getName());
+          }
+        }
+      }
+    }
+    return pathList;
+  }
+
 }
