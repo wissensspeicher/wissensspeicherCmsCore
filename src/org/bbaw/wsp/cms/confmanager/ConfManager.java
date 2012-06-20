7e2a7db472dc73b19ab212af7d1acc269be930a3
Josef Willenborg <willenborg@bbaw.de>
2012-06-20 15:16:01 +0000

    nun auf Saxon XPath basierend
    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@250 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/confmanager/ConfManager.java
+++ src/org/bbaw/wsp/cms/confmanager/ConfManager.java
@@ -2,27 +2,25 @@ package org.bbaw.wsp.cms.confmanager;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.bbaw.wsp.cms.harvester.PathExtractor;
+
+import de.mpg.mpiwg.berlin.mpdl.xml.xquery.XQueryEvaluator;
+
+import org.bbaw.wsp.cms.general.Constants;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
-
-import org.bbaw.wsp.cms.general.Constants;
 
 public class ConfManager {
 
@@ -54,111 +52,70 @@ public class ConfManager {
   private void checkCollectionConfFiles() throws XPathExpressionException {
     System.out.println("---------------");
     System.out.println("checking configuration files...");
-
-    // holt alle konfiguratiuonsdateien aus dem konf-Ordner
+    // holt alle Konfiguratiuonsdateien aus dem konf-Ordner
     PathExtractor ext = new PathExtractor();
     List<String> configsList = ext.extractPathLocally(Constants.getInstance().getConfDir());
-    System.out.println("Anzahl der konfugirationsdateien : " + configsList.size());
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    // docFactory.setNamespaceAware(true);
-    DocumentBuilder builder;
-
-    XPathFactory xPathFactory = XPathFactory.newInstance();
+    System.out.println("Anzahl der Konfigurationsdateien : " + configsList.size());
     try {
       File configFile = null;
-      // �berpr�ft alle konf-dateien auf update und f�hrt es bei bedarf aus
+      // Ueberprueft alle Konf-dateien auf update und fuehrt es bei Bedarf aus
       for (String configXml : configsList) {
         System.out.println("checking : " + configXml);
-        builder = docFactory.newDocumentBuilder();
+        XQueryEvaluator xQueryEvaluator = new XQueryEvaluator();
         configFile = new File(configXml);
-        Document document = null;
-        try{
-          document = builder.parse(configFile);
-        
-          // /////////////////
-          XPath xPath = xPathFactory.newXPath();
-          NodeList nl = document.getElementsByTagName("update");
-          XPathExpression updateExpr = xPath.compile("//collection/update/text()");
-          Object updateResult = updateExpr.evaluate(document, XPathConstants.STRING);
-          String updateNode = (String) updateResult;
-          // /////////////////
-  
-          if (updateNode != null) {
-            if(!updateNode.trim().equals("")){
-              org.w3c.dom.Node n = nl.item(0);
-              // String updateValue = n.getTextContent();
-              if (updateNode.trim().equals("true")) {
-    
-                System.out.println("update tag is set on : " + updateNode);
-                cmrw = new ConfManagerResultWrapper();
-    
-                NodeList idlist = document.getElementsByTagName("collectionId");
-                // darf jeweils nur ein node enthalten sein
-                Node idNode = idlist.item(0);
-                if (!idNode.getTextContent().equals("")) {
-                  cmrw.setCollectionId(idNode.getTextContent());
-                }
-                NodeList nodeliste = document.getElementsByTagName("mainLanguage");
-                // darf jeweils nur ein node enthalten sein
-                Node langNode = nodeliste.item(0);
-                if (!langNode.getTextContent().equals("")) {
-                  cmrw.setMainLanguage(langNode.getTextContent());
-                }
-    
-                NodeList collNamelist = document.getElementsByTagName("name");
-                // darf jeweils nur ein node enthalten sein
-                Node nameNode = collNamelist.item(0);
-                if (!nameNode.getTextContent().equals("")) {
-                  cmrw.setCollectionName(nameNode.getTextContent());
-                }
-    
-                NodeList fieldNodes = document.getElementsByTagName("field");
-                ArrayList<String> fields = new ArrayList<String>();
-                fields = new ArrayList<String>();
-                for (int i = 0; i < fieldNodes.getLength(); i++) {
-                  if (!fieldNodes.item(i).getTextContent().equals("")) {
-                    fields.add((fieldNodes.item(i).getTextContent().trim()));
-                  }
-                }
-                cmrw.setFields(fields);
-
-                NodeList nodeli = document.getElementsByTagName("collectionDataUrl");
-                Node node = nodeli.item(0);
-                if (!node.getTextContent().trim().equals("")) {
-                  cmrw.setCollectionDataUrl(node.getTextContent());
-                  extractUrlsFromCollections(node.getTextContent(), cmrw);
-                  
-                  // flag auf false setzen durch serialisierung in xml
-                  n.setTextContent("false");
-                  FileOutputStream os = new FileOutputStream(configFile);
-                  XMLSerializer ser = new XMLSerializer(os, null);
-                  ser.serialize(document);
-    
-                }
-                wrapperContainer.put(idNode.getTextContent(), cmrw);
-              }
-            }else{
-              // create Dummy
-              cmrw = new ConfManagerResultWrapper();
-              wrapperContainer.put("", cmrw);
-            }
-          }else{
-            //create Dummy
-            cmrw = new ConfManagerResultWrapper();
-            wrapperContainer.put("", cmrw);
+        URL srcUrl = configFile.toURI().toURL();
+        String update = xQueryEvaluator.evaluateAsString(srcUrl, "//collection/update/text()");
+        if (update != null && update.equals("true")) {
+          System.out.println("update tag is set on : " + update);
+          cmrw = new ConfManagerResultWrapper();
+          String collectionId = xQueryEvaluator.evaluateAsString(srcUrl, "//collectionId/text()");
+          if (collectionId != null) {
+            cmrw.setCollectionId(collectionId);
           }
-        }catch(Exception e){
-          e.printStackTrace();
-          System.out.println("no ressources to update");
+          String mainLanguage = xQueryEvaluator.evaluateAsString(srcUrl, "//mainLanguage/text()");
+          if (mainLanguage != null) {
+            cmrw.setMainLanguage(mainLanguage);
+          }
+          String name = xQueryEvaluator.evaluateAsString(srcUrl, "//name/text()");
+          if (name != null) {
+            cmrw.setCollectionName(name);
+          }
+          String fieldsStr = xQueryEvaluator.evaluateAsStringValueJoined(srcUrl, "//field");
+          ArrayList<String> fields = new ArrayList<String>();
+          if (fields != null) {
+            fieldsStr = fieldsStr.trim();
+            String[] fieldsArray = fieldsStr.split(" ");
+            for (int i=0; i<fieldsArray.length; i++) {
+              String field = fieldsArray[i];
+              fields.add(field);
+            }
+            cmrw.setFields(fields);
+          }
+          String collectionDataUrl = xQueryEvaluator.evaluateAsString(srcUrl, "//collectionDataUrl/text()");
+          if (collectionDataUrl != null) {
+            cmrw.setCollectionDataUrl(collectionDataUrl);
+            extractUrlsFromCollections(collectionDataUrl, cmrw);
+            // flag im Konfigurations-File auf false setzen durch serialisierung in das File
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = docFactory.newDocumentBuilder();
+            Document configFileDocument = builder.parse(configFile);
+            NodeList updateNodeList = configFileDocument.getElementsByTagName("update");
+            Node n = updateNodeList.item(0);
+            n.setTextContent("false");
+            FileOutputStream os = new FileOutputStream(configFile);
+            XMLSerializer ser = new XMLSerializer(os, null);
+            ser.serialize(configFileDocument);  //  Vorsicht: wenn es auf true ist: es wird alles neu indexiert
+          }
+          wrapperContainer.put(collectionId, cmrw);
         }
       }
-    } catch (ParserConfigurationException e1) {
-      e1.printStackTrace();
+    } catch(Exception e) {
+      e.printStackTrace();
     }
   }
 
   /**
-   * der extractor holt alle projekt zugeh�rigen Urls
+   * der Extractor holt alle Projekt zugehoerigen Urls
    */
   private void extractUrlsFromCollections(String collectionDataUrl, ConfManagerResultWrapper cmrw) {
     System.out.println("collecting urls of resources that need update...");
