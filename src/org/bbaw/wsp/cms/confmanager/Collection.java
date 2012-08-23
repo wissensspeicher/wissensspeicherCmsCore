8424a6ada2f60b8e18bbd6b8546c830b8cbc55dc
Josef Willenborg <willenborg@bbaw.de>
2012-08-23 10:17:02 +0000

    lesen der register wieder entfernt
    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@401 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/confmanager/Collection.java
+++ src/org/bbaw/wsp/cms/confmanager/Collection.java
@@ -1,13 +1,7 @@
 package org.bbaw.wsp.cms.confmanager;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-
-import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.util.URIUtil;
-
-import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
 
 public class Collection {
   private List<String> urls;
@@ -18,7 +12,6 @@ public class Collection {
   private ArrayList<String> fields;
   private List<String> formats;
   private List<String> excludeField;
-  private HashMap<String, ArrayList<String>> registers;
   
   public Collection(){
     urls = new ArrayList<String>();
@@ -29,7 +22,6 @@ public class Collection {
     fields = new ArrayList<String>();
     formats = new ArrayList<String>();
     excludeField = new ArrayList<String>();
-    registers = new HashMap<String, ArrayList<String>>();
   }
   
   public List<String> getUrls() {
@@ -95,40 +87,4 @@ public class Collection {
   public void setFields(ArrayList<String> fields) {
     this.fields = fields;
   }
-
-  public void setRegister(String registerName, ArrayList<String> entries) {
-    registers.put(registerName, entries);
-  }
-  
-  public ArrayList<String> getRegisterEntries(String registerName) {
-    return registers.get(registerName);
-  }
-
-  public String getRegisterLink(String registerName, String registerValue) throws ApplicationException {
-    String retRegLink = null;
-    ArrayList<String> regs = registers.get(registerName);
-    String reg = null;
-    if (regs != null && regs.size() >= 1) {
-      reg = regs.get(0);  // get first one
-      String valueQuery = "";
-      if (registerValue != null && ! registerValue.isEmpty()) {
-        valueQuery = valueQuery + "+tokenOrig:(";
-        String[] registerValueTerms = registerValue.split(" ");
-        for (int i=0; i<registerValueTerms.length; i++) {
-          String term = registerValueTerms[i];
-          valueQuery = valueQuery + " +" + term;
-        }
-        valueQuery = valueQuery + ")";
-      }
-      String query = "+elementName:" + registerName + " " + valueQuery;
-      try {
-        query = URIUtil.encodePath(query, "utf-8");
-      } catch (URIException e) {
-        throw new ApplicationException(e);
-      }
-      retRegLink = "/wspCmsWebApp/query/QueryDocument?docId=" + reg + "&query=" + query;
-    }
-    return retRegLink;
-  }
-
 }
