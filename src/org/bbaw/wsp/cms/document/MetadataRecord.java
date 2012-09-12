5c723d33a37fa4f289d9eb3aa79d6da3f070e72c
Josef Willenborg <willenborg@bbaw.de>
2012-09-12 15:43:07 +0000

    neue Felder für edoc
    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@424 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/document/MetadataRecord.java
+++ src/org/bbaw/wsp/cms/document/MetadataRecord.java
@@ -4,28 +4,100 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MetadataRecord {
-  private String docId;  // local id: document identifier in index system, e.g. /echo/la/Benedetti_1585.xml
-  private String identifier;  // local id: identifier field in documents metadata: e.g. /echo:echo/echo:metadata/dcterms:identifier  
-  private String uri;  // global id: document URI (uniform resource identifier), e.g. http://de.wikipedia.org/wiki/Ramones
+  private String docId; // local id: document identifier in index system, e.g. /echo/la/Benedetti_1585.xml
+  private String identifier; // local id: identifier field in documents metadata: e.g. /echo:echo/echo:metadata/dcterms:identifier
+  private String uri; // global id: document URI (uniform resource identifier), e.g. http://de.wikipedia.org/wiki/Ramones
   private String language;
-  private String creator;  // author
+  private String creator; // author
   private String title;
-  private String description;  // abstract etc.
-  private String subject;  // subject keywords from the title or description or content or subject lists (thesaurus etc.)
+  private String description; // abstract etc.
+  private String subject; // subject keywords from the title or description or content or subject lists (thesaurus etc.)
+  private String ddc; // Dewey Decimal Classification
+  private String swd; // Schlagwortnormdatei
   private String publisher; // publisher with place: e.g. Springer, New York
-  private String type; // mime type: e.g. text/xml  // TODO ist eigentlich das Feld "format" --> zus. instance variable "format" definieren
+  private String type; // mime type: e.g. text/xml // TODO ist eigentlich das Feld "format" --> zus. instance variable "format" definieren
   private String rights; // e.g. open access
   private Date date; // creation date, modification date, etc.
-  private String license;  // e.g. http://echo.mpiwg-berlin.mpg.de/policy/oa_basics/declaration
-  private String accessRights;  // e.g. free  
+  private String license; // e.g. http://echo.mpiwg-berlin.mpg.de/policy/oa_basics/declaration
+  private String accessRights; // e.g. free
   private String collectionNames; // e.g. "collection1 collection7"
   private String schemaName; // e.g. TEI, echo, html, or archimedes
   private Date lastModified;
   private int pageCount;
-  private String persons; 
-  private String places; 
-  private String echoId;  // document identifier in echo system: directory name, e.g. /permanent/library/163127KK  
- 
+  private String persons;
+  private String places;
+  private String echoId; // document identifier in echo system: directory name, e.g. /permanent/library/163127KK
+  private String urn; // e.g. the KOBV urn, e.g. urn:nbn:de:kobv:b4360-10020
+  private String documentType; // e.g. the KOBV "Dokumentenart"
+  private String isbn; // e.g. the KOBV ISBN
+  private Date creationDate; // e.g. the KOBV "Erstellungsjahr"
+  private Date publishingDate; // e.g. the KOBV "Publikationsdatum"
+  private String realDocUrl; // e.g. the URL to the eDoc (not the index.html)
+
+  public String getRealDocUrl() {
+    return realDocUrl;
+  }
+
+  public void setRealDocUrl(String realDocUrl) {
+    this.realDocUrl = realDocUrl;
+  }
+
+  public String getDocumentType() {
+    return documentType;
+  }
+
+  public void setDocumentType(String documentType) {
+    this.documentType = documentType;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public Date getPublishingDate() {
+    return publishingDate;
+  }
+
+  public void setPublishingDate(Date publishingDate) {
+    this.publishingDate = publishingDate;
+  }
+
+  public String getSwd() {
+    return swd;
+  }
+
+  public void setSwd(String swd) {
+    this.swd = swd;
+  }
+
+  public String getDdc() {
+    return ddc;
+  }
+
+  public void setDdc(String ddc) {
+    this.ddc = ddc;
+  }
+
+  public String getUrn() {
+    return urn;
+  }
+
+  public void setUrn(String urn) {
+    this.urn = urn;
+  }
+
   public String getDocId() {
     return docId;
   }
@@ -108,7 +180,7 @@ public class MetadataRecord {
     }
     return year;
   }
-  
+
   public String getDescription() {
     return description;
   }
@@ -205,4 +277,8 @@ public class MetadataRecord {
     this.echoId = echoId;
   }
 
+  @Override
+  public String toString() {
+    return "MetadataRecord [docId=" + docId + ", identifier=" + identifier + ", uri=" + uri + ", language=" + language + ", creator=" + creator + ", title=" + title + ", description=" + description + ", subject=" + subject + ", ddc=" + ddc + ", swd=" + swd + ", publisher=" + publisher + ", type=" + type + ", rights=" + rights + ", date=" + date + ", license=" + license + ", accessRights=" + accessRights + ", collectionNames=" + collectionNames + ", schemaName=" + schemaName + ", lastModified=" + lastModified + ", pageCount=" + pageCount + ", persons=" + persons + ", places=" + places + ", echoId=" + echoId + ", urn=" + urn + ", documentType=" + documentType + ", isbn=" + isbn + ", creationDate=" + creationDate + ", publishingDate=" + publishingDate + "]";
+  }
 }
