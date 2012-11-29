1c49f210f03311ac00134a2d66b6f54b4d592cf5
Sascha Feldmann <wsp-shk@bbaw.de>
2012-11-29 13:06:33 +0000

    * added warning logs.
    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsCore@624 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/mdsystem/metadata/convert2rdf/util/TemplateMapper.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/convert2rdf/util/TemplateMapper.java
@@ -1,7 +1,5 @@
 package org.bbaw.wsp.cms.mdsystem.metadata.convert2rdf.util;
 
-
-
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -13,10 +11,11 @@ import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
 
 /**
  * This class provides an easy to use template system.
+ * 
  * @author Sascha Feldmann (wsp-shk1)
  * 
- * Last change: 05.11.2012 - integration to wspcms
- *
+ *         Last change: 05.11.2012 - integration to wspcms
+ * 
  */
 public class TemplateMapper {
   private static IResourceReader reader = new ResourceReaderImpl();
@@ -25,44 +24,56 @@ public class TemplateMapper {
 
   /**
    * Create a new TemplateMapper for an input template.
-   * @param templateUrl - the url (local fileysystem or http) to the template.
-   * @throws ApplicationException if the value of the templateUrl is empty or null.
+   * 
+   * @param templateUrl
+   *          - the url (local fileysystem or http) to the template.
+   * @throws ApplicationException
+   *           if the value of the templateUrl is empty or null.
    */
   public TemplateMapper(final String templateUrl) throws ApplicationException {
-    if(templateUrl == null || templateUrl.isEmpty()) {
+    if (templateUrl == null || templateUrl.isEmpty()) {
       throw new ApplicationException("The value for the templateUrl musn't be null or empty.");
     }
     this.templateUrl = templateUrl;
     this.mappedTemplate = "";
   }
-  
+
   /**
    * Map the placeholders in the template with the given value.
-   * @param placeholderMap - a map with the placeholder as key and the value to be set as the value.
-   * @throws ApplicationException if the input stream for the template is not valid / available.
+   * 
+   * @param placeholderMap
+   *          - a map with the placeholder as key and the value to be set as the
+   *          value.
+   * @throws ApplicationException
+   *           if the input stream for the template is not valid / available.
    */
-  public void mapPlaceholder(final HashMap<String,String> placeholderMap) throws ApplicationException {
+  public void mapPlaceholder(final HashMap<String, String> placeholderMap) throws ApplicationException {
     InputStream in = reader.read(this.templateUrl);
-    Scanner scanner = new Scanner(in);   
+    Scanner scanner = new Scanner(in);
     scanner.useDelimiter("\n");
     StringBuilder builder = new StringBuilder();
     while (scanner.hasNext()) {
-      builder.append(scanner.next()+"\n"); 
+      builder.append(scanner.next() + "\n");
     }
-    String templateString = builder.toString();   
-    
-    for (String placeholder : placeholderMap.keySet()) {   
-      System.out.println("replacing "+placeholder+"by "+placeholderMap.get(placeholder));
-      
-     templateString = templateString.replaceAll(placeholder, placeholderMap.get(placeholder));
-    }   
+    String templateString = builder.toString();
+
+    for (String placeholder : placeholderMap.keySet()) {
+      System.out.println("replacing " + placeholder + "by " + placeholderMap.get(placeholder));
+      if (placeholderMap.get(placeholder).isEmpty()) { // print warning when a value is empty
+        System.err.println("WARNING: placeholder" + placeholder + " is empty");
+      }
+
+      templateString = templateString.replaceAll(placeholder, placeholderMap.get(placeholder));
+    }
     System.out.println("Mapped template");
     this.mappedTemplate = templateString;
   }
-  
+
   /**
    * Get the resulting string after mapping.
-   * @return the string with the mapped placeholders or an empty string if the map method wasn't called.
+   * 
+   * @return the string with the mapped placeholders or an empty string if the
+   *         map method wasn't called.
    */
   public String getMappedTemplate() {
     return this.mappedTemplate;
