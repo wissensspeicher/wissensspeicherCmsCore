081e9aa9929d6362bcf8ea13b1649c08bb32d5d2
Marco Juergens <juergens@bbaw.de>
2013-01-23 17:43:07 +0100

    merged mdsystem klassen

--- src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/RdfHandler.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/RdfHandler.java
@@ -30,10 +30,75 @@ import com.hp.hpl.jena.update.UpdateRequest;
 import com.hp.hpl.jena.util.FileManager;
 
 import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
-
+/**
+ * This class is designed to operate on jena tdb
+ * @author marco juergens
+ * @author sascha feldmann
+ * @author marco seidler
+ *
+ */
 public class RdfHandler {
 
+	/**
+	 * adds a certain statement without Sparql
+	 * if the ressource already exists nothing is added
+	 *  
+	 * @param model
+	 * @param uri
+	 * @param predicate
+	 * @param literalObj
+	 */
+	public void updateByJenaApi(Model model, String uri, String predicate, String literalObj){
+		System.out.println("adding ...");
+		model.add(
+			model.createResource(uri),
+			model.createProperty(predicate),
+			model.createLiteral(literalObj)
+		);
+		model.commit();
+		model.close();
+		System.out.println("ressource added");
+//      model.write(new PrintWriter(System.out));
+		model.write( System.out, "N3" );
+	}
 	
+	/**
+	 * removes a certain statement without Sparql 
+	 * @param model
+	 * @param uri
+	 * @param predicate
+	 * @param literalObj
+	 */
+	public void deleteByJenaApi(Model model, String uri, String predicate, String literalObj){
+		System.out.println("looking for matching statement to remove...");
+		Resource res = model.getResource(uri);
+		String resString = res.toString();
+		StmtIterator it = model.listStatements();
+		Statement statementToRemove = null;
+		while(it.hasNext()){
+			if(it.nextStatement().getObject() != null){
+				Statement st = it.nextStatement();
+				Resource  subject = st.getSubject();
+				if(subject.toString().equals(resString)){
+					if(st.getPredicate().toString().equals(predicate)){
+						if(st.getObject().toString().equals(literalObj)){
+							statementToRemove = st;
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		if(statementToRemove != null){
+			System.out.println("removing statement: "+statementToRemove );
+			model.remove(statementToRemove);
+		}else{
+			System.out.println("no matching statement found.");
+		}
+		model.close();
+	}
+
 	
 	/**
 	 * 
@@ -59,56 +124,15 @@ public class RdfHandler {
     }
 	
 	/**
-	 * 
-	 * 
-	 * @param store
-	 * @return
-	 */
-	public String getAllTriplesAsDot(Model model) {
-        StringBuffer dump = new StringBuffer();
-
-        StmtIterator statements = model.listStatements();
-        dump.append("digraph G { ");
-        dump.append('\n');
-        dump.append("edge [len=2];");
-        dump.append('\n');
-        while (statements.hasNext()) {
-            Statement statement = statements.nextStatement();
-        	System.out.println("statements.nextStatement() "+statement);
-            RDFNode object = statement.getObject();
-            dump.append("\"")
-            	.append(statement.getSubject().getLocalName())
-                .append("\" ")
-                .append("-> \"")
-                .append((object instanceof Resource || object instanceof Literal ?
-                     object.toString() : '"' + object.toString() + "\""))
-                .append("\"")
-                .append(" [label=\"")
-                .append(statement.getPredicate().getLocalName())
-                .append("\"]")
-                .append(";")
-                .append('\n');
-        }
-        dump.append('}');
-        return dump.toString();
-    }
-	
-	
-	/**
 	 * INSERT
 	 * @param store
 	 * @param queryString
 	 */
 	public void performUpdate(Dataset dataset, String queryString){
 		GraphStore graphStore = GraphStoreFactory.create(dataset) ;
-        
-		  UpdateRequest request = UpdateFactory.create(queryString) ;
-	        UpdateProcessor proc = UpdateExecutionFactory.create(request, graphStore) ;
-	        proc.execute() ;
-		
-//		System.out.println("model : "+model);
-//		System.out.println("query : "+queryString);
-		
+		UpdateRequest request = UpdateFactory.create(queryString) ;
+	    UpdateProcessor proc = UpdateExecutionFactory.create(request, graphStore) ;
+	    proc.execute() ;
 	}
 	
 	/**
@@ -173,101 +197,6 @@ public class RdfHandler {
 	public void readFileViaRiot(String filename, DatasetGraph dataset, Lang lang, String baseURI){
 		RiotLoader.read(filename, dataset, lang, baseURI);
 	}
-
-	/**
-	 * TODO
-	 * DELETE
-	 * @param store
-	 * @param queryString
-	 */
-	public void deleteRecord(Model model, String queryString){
-
-		GraphStore graphStore = GraphStoreFactory.create(model);
-		queryString = "DELETE WHERE { <http://avh.bbaw.de/biblio/Aggr> ?p ?o }";
-				
-				
-		/**
-		 * 	PREFIX dc: <http://purl.org/dc/elements/1.1/>
-
-			DELETE DATA FROM <http://example/bookStore>
-			{ <http://example/book3>  dc:title  "Fundamentals of Compiler Desing" }
-		 */
-		
-	}
-	
-	/**
-	 * TODO
-	 * DELETE all
-	 */
-	public void deleteAll(){
-		/**
-		 * 
-		 * 	DELETE
-			 { ?book ?p ?v }
-			WHERE
-			  { ?book dc:date ?date .
-			    FILTER ( ?date < "2000-01-01T00:00:00"^^xsd:dateTime )
-			    ?book ?p ?v
-			  }
-		 * 
-		 */
-	}
-	
-	/**
-	 * TODO
-	 * copy
-	 */
-	public void copyRecords(){
-		/**
-		 * von einem namedGraph zu einem anderen
-		 * 
-		 * 	PREFIX dc:  <http://purl.org/dc/elements/1.1/>
-			PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-			
-			INSERT INTO <http://example/bookStore2>
-			 { ?book ?p ?v }
-			WHERE
-			  { GRAPH  <http://example/bookStore>
-			       { ?book dc:date ?date .
-			         FILTER ( ?date < "2000-01-01T00:00:00"^^xsd:dateTime )
-			         ?book ?p ?v
-			  } }	
-		 * 
-		 */
-	}
-	
-	/**
-	 * TODO
-	 */
-	public void moveRecords(){
-		/**
-		 * 
-		 * 	PREFIX dc:  <http://purl.org/dc/elements/1.1/>
-			PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-			
-			INSERT INTO <http://example/bookStore2>
-			 { ?book ?p ?v }
-			WHERE
-			  { GRAPH  <http://example/bookStore>
-			     { ?book dc:date ?date . 
-			       FILTER ( ?date < "2000-01-01T00:00:00"^^xsd:dateTime )
-			       ?book ?p ?v
-			     }
-			  }
-			
-			DELETE FROM <http://example/bookStore>
-			 { ?book ?p ?v }
-			WHERE
-			  { GRAPH  <http://example/bookStore>
-			      { ?book dc:date ?date . 
-			        FILTER ( ?date < "2000-01-01T00:00:00"^^xsd:dateTime ) 
-			        ?book ?p ?v
-			      } 
-			  }   	
-		 * 
-		 */
-	}
-	
 	
 	/**
 	* Helper method that splits up a URI into a namespace and a local part.
@@ -295,57 +224,5 @@ public class RdfHandler {
 	    }
 	    split[1] = uri;
 	    return split;
-    }
-
-//	public void navigateTest(){
-//		// create an empty model
-//        Model model = ModelFactory.createDefaultModel();
-//       
-//        // use the FileManager to find the input file
-//        InputStream in = FileManager.get().open(oreTest);
-//        if (in == null) {
-//            throw new IllegalArgumentException( "File: " + oreTest + " not found");
-//        }
-//        
-//        // read the RDF/XML file
-//        model.read(new InputStreamReader(in), "");
-//        
-//        // retrieve the Adam Smith vcard resource from the model
-//        Resource descr = model.getResource("http://avh.bbaw.de/biblio/Aggr");
-//        
-//        Resource statem = (Resource )descr.getRequiredProperty(DC.title).getObject();
-//        
-//        System.out.println("statem : "+statem);
-//        
-//        // retrieve the value of the N property
-////        Resource name = (Resource) descr.getRequiredProperty(DCTerms.creator).getObject();
-//        
-//        Statement s = descr.getProperty(DCTerms.creator);
-//        System.out.println("as triple  : "+s.asTriple());
-//        System.out.println("creator s ? : "+s.getSubject());
-//        System.out.println("creator p ? : "+s.getPredicate());
-//        System.out.println("creator o ? : "+s);
-//        
-//        if (s instanceof Resource) {
-//        	System.out.println("creator o ? : "+s);
-//         } else {
-//             // object is a literal
-//             System.out.println(" \"" + s.toString() + "\"");
-//         }
-//        
-//        // retrieve the given name property
-//        String rights = descr.getRequiredProperty(DC.rights).getString();
-//        
-//        // add two nick name properties to vcard
-//        descr.addProperty(DCTerms.format, "PDF");
-//        
-//        // set up the output
-//        System.out.println("The rights of \"" + rights + "\" are:");
-//        // list the nicknames
-//        StmtIterator iter = descr.listProperties(DCTerms.format);
-//        while (iter.hasNext()) {
-//            System.out.println("    " + iter.nextStatement().getObject().toString());
-//        }
-//	}
-	   
+    }	   
 }
