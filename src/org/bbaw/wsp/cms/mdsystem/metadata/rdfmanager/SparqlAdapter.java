ff4308c14b878840398a351e34571301984cb2d5
Marco Juergens <juergens@bbaw.de>
2013-01-25 18:42:14 +0100

    architektur für JSON-Query an TDB

---
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/SparqlAdapter.java
@@ -0,0 +1,17 @@
+package org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager;
+
+/**
+ * This Class is meant to be the connector between @QueryHandler sent by the GUI and the @RdfHandler.
+ * It combines Strings to valid Sparql queries  
+ * 
+ * @author marco juergens
+ * 
+ */
+public class SparqlAdapter {
+	
+	/**
+	 * evtl. könnte diese Klasse eine abgewandelte Klasse von @FusekiClient werden 
+	 */
+	
+	
+}
