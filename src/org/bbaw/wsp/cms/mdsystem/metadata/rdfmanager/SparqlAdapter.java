4d79fbcf41b71a59c553ac9820217bc6534d390d
Sascha Feldmann <wsp-shk@bbaw.de>
2013-01-31 16:02:19 +0100

    * SparqlAdapter.class implemented. Not tested yet.

--- src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/SparqlAdapter.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/SparqlAdapter.java
@@ -1,17 +1,89 @@
 package org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager;
 
+import java.net.URL;
+
+import org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.tools.SparqlCommandBuilder;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.QueryExecution;
+
 /**
- * This Class is meant to be the connector between @QueryHandler sent by the GUI and the @RdfHandler.
- * It combines Strings to valid Sparql queries  
+ * This Class is meant to be the connector between @QueryHandler sent by the GUI
+ * and the @RdfHandler. It combines Strings to valid Sparql queries
  * 
  * @author marco juergens
  * 
  */
-public class SparqlAdapter {
-	
-	/**
-	 * evtl. könnte diese Klasse eine abgewandelte Klasse von @FusekiClient werden 
-	 */
-	
-	
+public class SparqlAdapter implements ISparqlAdapter {
+  private RdfHandler handler;
+  private Dataset dataset;
+
+  /**
+   * Create a new SparqlAdapter.
+   */
+  public SparqlAdapter(Dataset dataset) {
+    this.handler = new RdfHandler();
+    this.dataset = dataset;
+  }
+
+  @Override
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.ISparqlAdapter#buildSparqlQuery
+   * (java.lang.String)
+   */
+  public void buildSparqlQuery(String literal) {
+    String query = SparqlCommandBuilder.SELECT_USING_INDEX.getSelectQueryString("*", null, literal);
+    System.out.println("Builded query " + query);
+    QueryExecution ex = handler.selectSomething(query, this.dataset);
+    for (String bla : ex.execSelect().getResultVars()) {
+      System.out.println(bla);
+    }
+  }
+
+  @Override
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.ISparqlAdapter#buildSparqlQuery
+   * (java.net.URL, java.lang.String)
+   */
+  public void buildSparqlQuery(URL namedGraphUrl, String literal) {
+    String query = SparqlCommandBuilder.SELECT_NAMED.getSelectQueryString("*", namedGraphUrl.toExternalForm(), "?s ?p " + literal);
+    handler.selectSomething(query, dataset);
+  }
+
+  @Override
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.ISparqlAdapter#buildSparqlQuery
+   * (java.net.URL, java.net.URL, java.lang.String)
+   */
+  public void buildSparqlQuery(URL subject, URL predicate, String object) {
+    String query = SparqlCommandBuilder.SELECT_DEFAULT.getSelectQueryString("*", null, "<" + subject.toExternalForm() + "> <" + predicate.toExternalForm() + "> " + object);
+    handler.selectSomething(query, dataset);
+  }
+
+  @Override
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.ISparqlAdapter#
+   * findRelativeConcepts(java.net.URL)
+   */
+  public String findRelativeConcepts(URL subject) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * evtl. könnte diese Klasse eine abgewandelte Klasse von @FusekiClient
+   * werden
+   */
+
 }
