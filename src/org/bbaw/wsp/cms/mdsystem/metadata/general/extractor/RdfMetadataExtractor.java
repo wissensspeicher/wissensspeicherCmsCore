4a0c8ef3e1d91bf096acec0ffce42590972b8a50
GammaG <pain500@hotmail.de>
2013-02-07 14:47:31 +0100

    updates

--- src/org/bbaw/wsp/cms/mdsystem/metadata/general/extractor/RdfMetadataExtractor.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/general/extractor/RdfMetadataExtractor.java
@@ -2,6 +2,8 @@ package org.bbaw.wsp.cms.mdsystem.metadata.general.extractor;
 
 import java.util.HashMap;
 
+import org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.QueryTarget;
+
 import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
 
 /**
@@ -57,11 +59,195 @@ public class RdfMetadataExtractor extends MetadataExtractor {
 		String number = (String) buildXPath("count(//rdf:Description)", false);
 
 		int count = Integer.parseInt(number);
-		System.out.println(count);
+		for (int i = 1; i <= count; ++i) {
+			// String temp = queryExecute("//rdf:Description[" + i
+			// + "]/@rdf:about");
+			if (checkIfUsefull(i, element)) {
+				QueryTarget target = new QueryTarget();
 
-		// result.add((String) buildXPath("//rdf:Description[" + (i + 1)
-		// + "]/rdf:type/@rdf:resource", false)); // First
+				String query = "//rdf:Description[" + i + "]/@rdf:about";
+				addToTarget("Description", queryExecute(query), target);
 
+				query = "//rdf:Description[" + i
+						+ "]/owl:versionInfo/@rdf:datatype";
+				addToTarget("versionInfo", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/rdf:type/@rdf:resource";
+				addToTarget("type", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/owl:imports/@rdf:resource";
+				addToTarget("imports", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dcterms:isPartOf/@rdf:resource";
+				addToTarget("isPartOf", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dc:description/@rdf:datatype";
+				addToTarget("description", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/rdfs:label/@rdf:datatype";
+				addToTarget("label", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/gnd:languageCode/@rdf:datatype";
+				addToTarget("languageCode", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/foaf:familyName/@rdf:datatype";
+				addToTarget("familyName", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/foaf:mbox/@rdf:datatype";
+				addToTarget("mbox", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/foaf:givenName/@rdf:datatype";
+				addToTarget("givenName", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/foaf:title/@rdf:datatype";
+				addToTarget("title", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/@gnd:functionOrRole";
+				addToTarget("functionOrRole", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/@gnd:definition";
+				addToTarget("definition", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/foaf:status/@rdf:datatype";
+				addToTarget("status", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dc:coverage/@rdf:resource";
+				addToTarget("coverage", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/gnd:topic/@rdf:datatype";
+				addToTarget("topic", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/foaf:homepage/@rdf:resource";
+				addToTarget("homepage", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dcterms:valid/@rdf:datatype";
+				addToTarget("valid", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/foaf:name/@rdf:datatype";
+				addToTarget("name", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dc:contributor/@rdf:resource";
+				addToTarget("contributor", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i + "]/foaf:nick/@rdf:datatype";
+				addToTarget("nick", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dcterms:isReplacedBy/@rdf:resource";
+				addToTarget("isReplacedBy", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/gnd:gndIdentifier/@rdf:datatype";
+				addToTarget("gndIdentifier", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/gnd:relatedCorporateBody/@rdf:resource";
+				addToTarget("relatedCorporateBody", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/gnd:contributingCorporateBody/@rdf:resource";
+				addToTarget("contributingCorporateBody", queryExecute(query),
+						target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dcterms:temporal/@rdf:datatype";
+				addToTarget("temporal", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/dcterms:requires/@rdf:resource";
+				addToTarget("requires", queryExecute(query), target);
+
+				query = "//rdf:Description[" + i
+						+ "]/foaf:fundedBy/@rdf:resource";
+				addToTarget("fundedBy", queryExecute(query), target);
+			}
+		}
+
+	}
+
+	private void addToTarget(String key, String query, QueryTarget target) {
+		if (query != null && !query.equals(""))
+			target.addToMap(key, query);
+	}
+
+	private String queryExecute(String query) {
+		return (String) buildXPath(query, false);
+	}
+
+	private Boolean checkIfUsefull(int i, String element) {
+
+		String temp = "";
+		temp += queryExecute("//rdf:Description[" + i + "]/@rdf:about");
+
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/owl:versionInfo/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/rdf:type/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/owl:imports/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dcterms:isPartOf/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dc:description/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/rdfs:label/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/gnd:languageCode/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:familyName/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:mbox/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:mbox/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:givenName/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:title/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i + "]/@gnd:functionOrRole");
+		temp += queryExecute("//rdf:Description[" + i + "]/@gnd:definition");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:status/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dc:coverage/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/gnd:topic/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:homepage/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dcterms:valid/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:name/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dc:contributor/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:nick/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dcterms:isReplacedBy/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/gnd:gndIdentifier/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/gnd:relatedCorporateBody/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/gnd:contributingCorporateBody/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dcterms:temporal/@rdf:datatype");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/dcterms:requires/@rdf:resource");
+		temp += queryExecute("//rdf:Description[" + i
+				+ "]/foaf:fundedBy/@rdf:resource");
+
+		return temp.contains(element);
 	}
 
 	/**
--- src/org/bbaw/wsp/cms/mdsystem/util/MdsystemConfigRecord.java
+++ src/org/bbaw/wsp/cms/mdsystem/util/MdsystemConfigRecord.java
@@ -8,137 +8,151 @@ package org.bbaw.wsp.cms.mdsystem.util;
  * 
  */
 public class MdsystemConfigRecord implements IMdsystemConfigRecord {
-  private String eDocTemplatePath;
-  private String modsStylesheetPath;
-  private String metsStylesheetPath;
-  private String datasetPath;
-  private String larqIndexPath;
-  private String normdataPath;
+	private String eDocTemplatePath;
+	private String modsStylesheetPath;
+	private String metsStylesheetPath;
+	private String datasetPath;
+	private String larqIndexPath;
+	private String normdataPath;
 
-  public void setNormdataPath(String normdataPath) {
-    this.normdataPath = normdataPath;
-  }
+	public void setNormdataPath(String normdataPath) {
+		this.normdataPath = normdataPath;
+	}
 
-  @Override
-  public String geteDocTemplatePath() {
-    return eDocTemplatePath;
-  }
+	@Override
+	public String geteDocTemplatePath() {
+		return eDocTemplatePath;
+	}
 
-  public void seteDocTemplatePath(final String eDocTemplatePath) {
-    this.eDocTemplatePath = eDocTemplatePath;
-  }
+	public void seteDocTemplatePath(final String eDocTemplatePath) {
+		this.eDocTemplatePath = eDocTemplatePath;
+	}
 
-  @Override
-  public String getModsStylesheetPath() {
-    return modsStylesheetPath;
-  }
+	@Override
+	public String getModsStylesheetPath() {
+		return modsStylesheetPath;
+	}
 
-  public void setModsStylesheetPath(final String modsStylesheetPath) {
-    this.modsStylesheetPath = modsStylesheetPath;
-  }
+	public void setModsStylesheetPath(final String modsStylesheetPath) {
+		this.modsStylesheetPath = modsStylesheetPath;
+	}
 
-  @Override
-  public String getMetsStylesheetPath() {
-    return metsStylesheetPath;
-  }
+	@Override
+	public String getMetsStylesheetPath() {
+		return metsStylesheetPath;
+	}
 
-  public void setMetsStylesheetPath(final String metsStylesheetPath) {
-    this.metsStylesheetPath = metsStylesheetPath;
-  }
+	public void setMetsStylesheetPath(final String metsStylesheetPath) {
+		this.metsStylesheetPath = metsStylesheetPath;
+	}
 
-  @Override
-  public String getDatasetPath() {
-    return datasetPath;
-  }
+	@Override
+	public String getDatasetPath() {
+		return datasetPath;
+	}
 
-  public void setDatasetPath(final String datasetPath) {
-    this.datasetPath = datasetPath;
-  }
+	public void setDatasetPath(final String datasetPath) {
+		this.datasetPath = datasetPath;
+	}
 
-  @Override
-  public String getLarqIndexPath() {
-    return larqIndexPath;
-  }
+	@Override
+	public String getLarqIndexPath() {
+		return larqIndexPath;
+	}
 
-  public void setLarqIndexPath(final String larqIndexPath) {
-    this.larqIndexPath = larqIndexPath;
-  }
+	public void setLarqIndexPath(final String larqIndexPath) {
+		this.larqIndexPath = larqIndexPath;
+	}
 
-  @Override
-  public String toString() {
-    return "MdsystemConfigRecord [eDocTemplatePath=" + eDocTemplatePath + ", modsStylesheetPath=" + modsStylesheetPath + ", metsStylesheetPath=" + metsStylesheetPath + ", datasetPath=" + datasetPath + ", larqIndexPath=" + larqIndexPath + ", normdataPath=" + normdataPath + "]";
-  }
+	@Override
+	public String toString() {
+		return "MdsystemConfigRecord [eDocTemplatePath=" + eDocTemplatePath
+				+ ", modsStylesheetPath=" + modsStylesheetPath
+				+ ", metsStylesheetPath=" + metsStylesheetPath
+				+ ", datasetPath=" + datasetPath + ", larqIndexPath="
+				+ larqIndexPath + ", normdataPath=" + normdataPath + "]";
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((datasetPath == null) ? 0 : datasetPath.hashCode());
-    result = prime * result + ((eDocTemplatePath == null) ? 0 : eDocTemplatePath.hashCode());
-    result = prime * result + ((larqIndexPath == null) ? 0 : larqIndexPath.hashCode());
-    result = prime * result + ((metsStylesheetPath == null) ? 0 : metsStylesheetPath.hashCode());
-    result = prime * result + ((modsStylesheetPath == null) ? 0 : modsStylesheetPath.hashCode());
-    return result;
-  }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((datasetPath == null) ? 0 : datasetPath.hashCode());
+		result = prime
+				* result
+				+ ((eDocTemplatePath == null) ? 0 : eDocTemplatePath.hashCode());
+		result = prime * result
+				+ ((larqIndexPath == null) ? 0 : larqIndexPath.hashCode());
+		result = prime
+				* result
+				+ ((metsStylesheetPath == null) ? 0 : metsStylesheetPath
+						.hashCode());
+		result = prime
+				* result
+				+ ((modsStylesheetPath == null) ? 0 : modsStylesheetPath
+						.hashCode());
+		return result;
+	}
 
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final MdsystemConfigRecord other = (MdsystemConfigRecord) obj;
-    if (datasetPath == null) {
-      if (other.datasetPath != null) {
-        return false;
-      }
-    } else if (!datasetPath.equals(other.datasetPath)) {
-      return false;
-    }
-    if (eDocTemplatePath == null) {
-      if (other.eDocTemplatePath != null) {
-        return false;
-      }
-    } else if (!eDocTemplatePath.equals(other.eDocTemplatePath)) {
-      return false;
-    }
-    if (larqIndexPath == null) {
-      if (other.larqIndexPath != null) {
-        return false;
-      }
-    } else if (!larqIndexPath.equals(other.larqIndexPath)) {
-      return false;
-    }
-    if (metsStylesheetPath == null) {
-      if (other.metsStylesheetPath != null) {
-        return false;
-      }
-    } else if (!metsStylesheetPath.equals(other.metsStylesheetPath)) {
-      return false;
-    }
-    if (modsStylesheetPath == null) {
-      if (other.modsStylesheetPath != null) {
-        return false;
-      }
-    } else if (!modsStylesheetPath.equals(other.modsStylesheetPath)) {
-      return false;
-    }
-    return true;
-  }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final MdsystemConfigRecord other = (MdsystemConfigRecord) obj;
+		if (datasetPath == null) {
+			if (other.datasetPath != null) {
+				return false;
+			}
+		} else if (!datasetPath.equals(other.datasetPath)) {
+			return false;
+		}
+		if (eDocTemplatePath == null) {
+			if (other.eDocTemplatePath != null) {
+				return false;
+			}
+		} else if (!eDocTemplatePath.equals(other.eDocTemplatePath)) {
+			return false;
+		}
+		if (larqIndexPath == null) {
+			if (other.larqIndexPath != null) {
+				return false;
+			}
+		} else if (!larqIndexPath.equals(other.larqIndexPath)) {
+			return false;
+		}
+		if (metsStylesheetPath == null) {
+			if (other.metsStylesheetPath != null) {
+				return false;
+			}
+		} else if (!metsStylesheetPath.equals(other.metsStylesheetPath)) {
+			return false;
+		}
+		if (modsStylesheetPath == null) {
+			if (other.modsStylesheetPath != null) {
+				return false;
+			}
+		} else if (!modsStylesheetPath.equals(other.modsStylesheetPath)) {
+			return false;
+		}
+		return true;
+	}
 
-  @Override
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.bbaw.wsp.cms.mdsystem.util.IMdsystemConfigRecord#getNormdataPath()
-   */
-  public String getNormdataPath() {
-    return this.normdataPath;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.bbaw.wsp.cms.mdsystem.util.IMdsystemConfigRecord#getNormdataPath()
+	 */
+	public String getNormdataPath() {
+		return this.normdataPath;
+	}
 
 }
