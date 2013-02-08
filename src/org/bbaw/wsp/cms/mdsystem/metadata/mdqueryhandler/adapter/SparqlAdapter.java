f38b9e668a867c7a6b3d0d8ed2e63099d713779c
Sascha Feldmann <wsp-shk@bbaw.de>
2013-02-08 12:49:08 +0100

    * Removed errors.

--- src/org/bbaw/wsp/cms/mdsystem/metadata/mdqueryhandler/adapter/SparqlAdapter.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/mdqueryhandler/adapter/SparqlAdapter.java
@@ -86,7 +86,6 @@ public class SparqlAdapter<T> implements ISparqlAdapter {
 
   }
 
-  @Override
   /*
    * (non-Javadoc)
    * 
@@ -94,25 +93,28 @@ public class SparqlAdapter<T> implements ISparqlAdapter {
    * org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.ISparqlAdapter#buildSparqlQuery
    * (java.net.URL, java.lang.String)
    */
-  public void buildSparqlQuery(final URL namedGraphUrl, final String literal) {
-    final String query = SparqlCommandBuilder.SELECT_NAMED_USING_INDEX.getSelectQueryString("*", namedGraphUrl.toExternalForm(), literal);
-    System.out.println("Builded query " + query);
-    final ResultSet results = delegateQuery(query);
-    final StmtIterator it = results.getResourceModel().listStatements();
-
-    // while (it.hasNext()) {
-    // final Statement statement = it.next();
-    // System.out.println(statement);
-    // }
-
-    while (results.hasNext()) { // hier kommt man nicht weiter, wie an die
-      // Tripels ran???
-      final QuerySolution solution = results.next();
-      // System.out.println(solution);
-      // System.out.println("s: " + solution.getResource("s"));
-      System.out.println("o: " + solution.getLiteral("s"));
-    }
-  }
+  // public void buildSparqlQuery(final URL namedGraphUrl, final String literal)
+  // {
+  // final String query =
+  // SparqlCommandBuilder.SELECT_NAMED_USING_INDEX.getSelectQueryString("*",
+  // namedGraphUrl.toExternalForm(), literal);
+  // System.out.println("Builded query " + query);
+  // final ResultSet results = delegateQuery(query);
+  // final StmtIterator it = results.getResourceModel().listStatements();
+  //
+  // // while (it.hasNext()) {
+  // // final Statement statement = it.next();
+  // // System.out.println(statement);
+  // // }
+  //
+  // while (results.hasNext()) { // hier kommt man nicht weiter, wie an die
+  // // Tripels ran???
+  // final QuerySolution solution = results.next();
+  // // System.out.println(solution);
+  // // System.out.println("s: " + solution.getResource("s"));
+  // System.out.println("o: " + solution.getLiteral("s"));
+  // }
+  // }
 
   @Override
   /*
@@ -123,13 +125,16 @@ public class SparqlAdapter<T> implements ISparqlAdapter {
    * (java.net.URL, java.net.URL, java.lang.String)
    */
   public void buildSparqlQuery(final URL subject, final URL predicate, final String object) {
-    final String query = SparqlCommandBuilder.SELECT_DEFAULT.getSelectQueryString("*", null, "<" + subject.toExternalForm() + "> <" + predicate.toExternalForm() + "> " + object);
-    System.out.println("Builded query " + query);
-    final QueryExecution ex = delegateQuery(query);
-    handler.selectSomething(query, dataset);
-    for (final String bla : ex.execSelect().getResultVars()) {
-      System.out.println(bla);
-    }
+    // final String query =
+    // SparqlCommandBuilder.SELECT_DEFAULT.getSelectQueryString("*", null, "<" +
+    // subject.toExternalForm() + "> <" + predicate.toExternalForm() + "> " +
+    // object);
+    // System.out.println("Builded query " + query);
+    // final QueryExecution ex = delegateQuery(query);
+    // handler.selectSomething(query, dataset);
+    // for (final String bla : ex.execSelect().getResultVars()) {
+    // System.out.println(bla);
+    // }
   }
 
   @Override
@@ -153,27 +158,36 @@ public class SparqlAdapter<T> implements ISparqlAdapter {
    * (java.net.URL, java.net.URL, java.net.URL, java.lang.String)
    */
   public void buildSparqlQuery(final URL namedGraphUrl, final URL subject, final URL predicate, final String object) {
-    final String query = SparqlCommandBuilder.SELECT_NAMED.getSelectQueryString("*", namedGraphUrl.toExternalForm(), "<" + subject.toExternalForm() + "> <" + predicate.toExternalForm() + "> " + object);
-    System.out.println("Builded query " + query);
-    final QueryExecution ex = delegateQuery(query);
-    handler.selectSomething(query, dataset);
-    for (final String bla : ex.execSelect().getResultVars()) {
-      System.out.println(bla);
-    }
-
-    final Query q = QueryFactory.create(query);
-    System.out.println("Builded query " + query);
-    final QueryExecution qExec = QueryExecutionFactory.create(q, dataset);
-    // for (String bla : ex.execSelect().getResultVars()) {
-    //
-    // // System.out.println(bla);
+    // final String query =
+    // SparqlCommandBuilder.SELECT_NAMED.getSelectQueryString("*",
+    // namedGraphUrl.toExternalForm(), "<" + subject.toExternalForm() + "> <" +
+    // predicate.toExternalForm() + "> " + object);
+    // System.out.println("Builded query " + query);
+    // final QueryExecution ex = delegateQuery(query);
+    // handler.selectSomething(query, dataset);
+    // for (final String bla : ex.execSelect().getResultVars()) {
+    // System.out.println(bla);
     // }
-    ResultSetFormatter.out(System.out, qExec.execSelect(), q);
+    //
+    // final Query q = QueryFactory.create(query);
+    // System.out.println("Builded query " + query);
+    // final QueryExecution qExec = QueryExecutionFactory.create(q, dataset);
+    // // for (String bla : ex.execSelect().getResultVars()) {
+    // //
+    // // // System.out.println(bla);
+    // // }
+    // ResultSetFormatter.out(System.out, qExec.execSelect(), q);
   }
 
-  public HitRecordContainer getHitRecordContainer() {
-    return hitRecordContainer;
+  @Override
+  public void buildSparqlQuery(final URL namedGraphUrl, final String literal) {
+    // TODO Auto-generated method stub
+
   }
+
+  // public HitRecordContainer getHitRecordContainer() {
+  // // return hitRecordContainer;
+  // }
 
   /**
    * evtl. könnte diese Klasse eine abgewandelte Klasse von @FusekiClient
