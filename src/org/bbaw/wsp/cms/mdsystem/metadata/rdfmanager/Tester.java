c84e4f44371a7113d92e6d7d68690c3ad9c384ea
Marco Seidler <wsp-shk2@bbaw.de>
2013-02-11 11:58:27 +0100

    update

---
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/Tester.java
@@ -0,0 +1,19 @@
+package org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager;
+
+import org.bbaw.wsp.cms.mdsystem.metadata.mdqueryhandler.ConceptIdentfierSearchMode;
+import org.bbaw.wsp.cms.mdsystem.metadata.mdqueryhandler.ConceptIdentifier;
+
+public class Tester {
+
+	/**
+	 * TestClass
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ConceptIdentifier().initIdentifying("Karl Marx",
+				ConceptIdentfierSearchMode.METHODE_OR);
+
+	}
+
+}
