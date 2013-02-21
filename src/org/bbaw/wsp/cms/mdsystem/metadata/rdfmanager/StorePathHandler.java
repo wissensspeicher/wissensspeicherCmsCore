de3fc94c4e6db22c09f50238ed3a0b742a2a6bdf
Marco Seidler <wsp-shk2@bbaw.de>
2013-02-21 16:17:25 +0100

    new class

---
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/rdfmanager/StorePathHandler.java
@@ -0,0 +1,17 @@
+package org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager;
+
+import java.io.Serializable;
+
+public class StorePathHandler implements Serializable {
+
+	private String path = "";
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
