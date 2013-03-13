9b9bdf551dc3dda8c250512af32e0b217acd3209
Marco Juergens <juergens@bbaw.de>
2013-03-13 11:51:07 +0100

    asdf

---
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/mdqueryhandler/ConceptQueryWrapper.java
@@ -0,0 +1,59 @@
+package org.bbaw.wsp.cms.mdsystem.metadata.mdqueryhandler;
+
+import java.util.ArrayList;
+
+/**
+ * Manages a list of @QueryTarget
+ * 
+ * @author shk2
+ * 
+ */
+public class ConceptQueryWrapper {
+
+	private final ArrayList<ConceptQueryResult> list = new ArrayList<ConceptQueryResult>();
+	private static ConceptQueryWrapper instance;
+
+	private ConceptQueryWrapper() {
+		list.clear();
+	}
+
+	/**
+	 * returns the instance of @QueryLibary
+	 * 
+	 * @return
+	 */
+	public static ConceptQueryWrapper getInstance() {
+		if (instance == null)
+			instance = new ConceptQueryWrapper();
+		return instance;
+	}
+
+	/**
+	 * adds an given @QueryTarget to the managed List
+	 * 
+	 * @param target
+	 */
+	public void addToList(ConceptQueryResult target) {
+		if (target != null) {
+			list.add(target);
+		}
+	}
+
+	/**
+	 * returns the actually given list of elements
+	 * 
+	 * @return
+	 */
+	public ArrayList<ConceptQueryResult> getQueryTargetList() {
+		return list;
+
+	}
+
+	/**
+	 * Clears the list of Elements, should be used before a new query
+	 */
+	public void ClearElements() {
+		list.clear();
+	}
+
+}
