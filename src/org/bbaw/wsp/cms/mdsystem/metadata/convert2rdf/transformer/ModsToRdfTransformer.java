675e144503e85aa9d6cc13724a9dded038f31483
Marco Seidler <wsp-shk2@bbaw.de>
2013-04-16 11:41:13 +0200

    little bugfix

--- src/org/bbaw/wsp/cms/mdsystem/metadata/convert2rdf/transformer/ModsToRdfTransformer.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/convert2rdf/transformer/ModsToRdfTransformer.java
@@ -3,7 +3,7 @@
  */
 package org.bbaw.wsp.cms.mdsystem.metadata.convert2rdf.transformer;
 
-import org.bbaw.wsp.cms.mdsystem.util.MdystemConfigReader;
+import org.bbaw.wsp.cms.mdsystem.util.MdSystemConfigReader;
 
 import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
 
@@ -15,40 +15,42 @@ import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
  * @date 08.10.12
  * 
  */
-public class ModsToRdfTransformer extends ToRdfTransformer implements IXsltTransformable {
-  /**
-   * The standard XSLT template for mods to rdf transformations. Use
-   * setXslInput() if you prefer another template.
-   */
-  public static final String PATH_XSLT_TEMPLATE = MdystemConfigReader.getInstance().getConfig().getModsStylesheetPath();
+public class ModsToRdfTransformer extends ToRdfTransformer implements
+		IXsltTransformable {
+	/**
+	 * The standard XSLT template for mods to rdf transformations. Use
+	 * setXslInput() if you prefer another template.
+	 */
+	public static final String PATH_XSLT_TEMPLATE = MdSystemConfigReader
+			.getInstance().getConfig().getModsStylesheetPath();
 
-  private static ModsToRdfTransformer instance;
+	private static ModsToRdfTransformer instance;
 
-  private ModsToRdfTransformer() throws ApplicationException {
-    super(ToRdfTransformer.MODE_XSLT); // this is an XSLT transformer
-    setXslInput(PATH_XSLT_TEMPLATE);
-  }
+	private ModsToRdfTransformer() throws ApplicationException {
+		super(ToRdfTransformer.MODE_XSLT); // this is an XSLT transformer
+		setXslInput(PATH_XSLT_TEMPLATE);
+	}
 
-  /**
-   * 
-   * @return the only existing instance.
-   * @throws ApplicationException
-   *           if the mode wasn't specified correctly.
-   */
-  public static ModsToRdfTransformer getInstance() throws ApplicationException {
-    if (instance == null) {
-      return new ModsToRdfTransformer();
-    }
-    return instance;
-  }
+	/**
+	 * 
+	 * @return the only existing instance.
+	 * @throws ApplicationException
+	 *           if the mode wasn't specified correctly.
+	 */
+	public static ModsToRdfTransformer getInstance() throws ApplicationException {
+		if (instance == null) {
+			return new ModsToRdfTransformer();
+		}
+		return instance;
+	}
 
-  /**
-   * Set the aggregation id. This will be set to the xslt transformation.
-   * 
-   * @param id
-   */
-  public void setAggregationId(final int id) {
-    super.aggrId = id;
-  }
+	/**
+	 * Set the aggregation id. This will be set to the xslt transformation.
+	 * 
+	 * @param id
+	 */
+	public void setAggregationId(final int id) {
+		super.aggrId = id;
+	}
 
 }
