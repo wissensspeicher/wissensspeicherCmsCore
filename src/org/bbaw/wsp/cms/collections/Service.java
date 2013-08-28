8247387463e3560d7ea89156678b13bf42ad4711
joey <willenborg@bbaw.de>
2013-08-28 18:04:56 +0200

    neue Service Klasse

---
+++ src/org/bbaw/wsp/cms/collections/Service.java
@@ -0,0 +1,58 @@
+package org.bbaw.wsp.cms.collections;
+
+import java.util.Hashtable;
+
+public class Service {
+  private String id;
+  private String hostName;
+  private String name;
+  private Hashtable<String, String> parameters;
+
+  public String toUrlStr() {
+    String urlStr = "";
+    if (hostName != null)
+      urlStr = urlStr + "http://" + hostName;
+    if (name != null)
+      urlStr = urlStr + name;
+    return urlStr;
+  }
+  
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getHostName() {
+    return hostName;
+  }
+
+  public void setHostName(String hostName) {
+    this.hostName = hostName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Hashtable<String, String> getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(Hashtable<String, String> parameters) {
+    this.parameters = parameters;
+  }
+
+  public String getParamValue(String paramName) {
+    if (parameters == null)
+      return null;
+    else return parameters.get(paramName);
+  }
+  
+}
