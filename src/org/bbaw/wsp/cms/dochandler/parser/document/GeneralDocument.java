b0532bc99753a20db7da41d82e4715a365218988
Sascha Feldmann <wsp-shk@bbaw.de>
2013-11-26 16:22:26 +0100

    - added exception for '.docx' files because TIKA doesn't support Microsoft Word 2007+ .docx format. - increased toString of GeneralDocument

--- src/org/bbaw/wsp/cms/dochandler/parser/document/GeneralDocument.java
+++ src/org/bbaw/wsp/cms/dochandler/parser/document/GeneralDocument.java
@@ -18,79 +18,88 @@ import org.bbaw.wsp.cms.document.MetadataRecord;
  */
 public class GeneralDocument implements IDocument {
 
-  private String textOrig;
-  private String url;
-  protected MetadataRecord metadata;
+	private String textOrig;
+	private String url;
+	protected MetadataRecord metadata;
 
-  /**
-   * Create a new GeneralDocument model class.
-   * 
-   * @param url
-   *          - URL of the parsed document.
-   * @param fulltext
-   *          - the parsed fulltext.
-   * @throws IllegalArgumentException
-   *           if one of the parameters is null.
-   */
-  public GeneralDocument(final String url, final String fulltext) {
-    if (url == null) {
-      throw new IllegalArgumentException("The value for the parameter URL in GeneralDocument mustn't be null.");
-    }
-    if (fulltext == null) {
-      throw new IllegalArgumentException("The value for the parameter fulltext in GeneralDocument mustn't be null.");
-    }
+	/**
+	 * Create a new GeneralDocument model class.
+	 * 
+	 * @param url
+	 *            - URL of the parsed document.
+	 * @param fulltext
+	 *            - the parsed fulltext.
+	 * @throws IllegalArgumentException
+	 *             if one of the parameters is null.
+	 */
+	public GeneralDocument(final String url, final String fulltext) {
+		if (url == null) {
+			throw new IllegalArgumentException(
+					"The value for the parameter URL in GeneralDocument mustn't be null.");
+		}
+		if (fulltext == null) {
+			throw new IllegalArgumentException(
+					"The value for the parameter fulltext in GeneralDocument mustn't be null.");
+		}
 
-    this.url = url;
-    this.textOrig = fulltext;
-  }
+		this.url = url;
+		this.textOrig = fulltext;
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see bbaw.wsp.parser.fulltext.document.IDocument#getFulltext()
-   */
-  public String getTextOrig() {
-    return this.textOrig;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bbaw.wsp.parser.fulltext.document.IDocument#getFulltext()
+	 */
+	public String getTextOrig() {
+		return this.textOrig;
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see bbaw.wsp.parser.fulltext.document.IDocument#getURL()
-   */
-  public String getURL() {
-    return this.url;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bbaw.wsp.parser.fulltext.document.IDocument#getURL()
+	 */
+	public String getURL() {
+		return this.url;
+	}
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#toString()
-   */
-  public String toString() {
-    return "GeneralDocument [fulltext=" + textOrig + ", url=" + url + "]";
-  }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("GeneralDocument [textOrig=");
+		builder.append(textOrig);
+		builder.append(", url=");
+		builder.append(url);
+		builder.append(", metadata=");
+		builder.append(metadata);
+		builder.append("]");
+		return builder.toString();
+	}
 
-  /*
-   * (non-Javadoc)
-   * @see org.bbaw.wsp.cms.dochandler.parser.document.IDocument#getMetadata()
-   */
-  public MetadataRecord getMetadata() {
-    return this.metadata;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bbaw.wsp.cms.dochandler.parser.document.IDocument#getMetadata()
+	 */
+	public MetadataRecord getMetadata() {
+		return this.metadata;
+	}
 
-  public void setMetadata(final MetadataRecord metadata) {
-    if (metadata.getUri() == null) {
-      metadata.setUri(this.getURL());
-    }
-    this.metadata = metadata;
-  }
+	public void setMetadata(final MetadataRecord metadata) {
+		if (metadata.getUri() == null) {
+			metadata.setUri(this.getURL());
+		}
+		this.metadata = metadata;
+	}
 
-  /**
-   * Set the textOrig. This is necessary if the sub classes of {@link ResourceParser} need to change an recieved fulltext.
-   * @param textOrig
-   */
-  public void setTextOrig(final String textOrig) {
-    this.textOrig = textOrig;
-  }
+	/**
+	 * Set the textOrig. This is necessary if the sub classes of
+	 * {@link ResourceParser} need to change an recieved fulltext.
+	 * 
+	 * @param textOrig
+	 */
+	public void setTextOrig(final String textOrig) {
+		this.textOrig = textOrig;
+	}
 }
--- src/org/bbaw/wsp/cms/dochandler/parser/text/parser/DocumentParser.java
+++ src/org/bbaw/wsp/cms/dochandler/parser/text/parser/DocumentParser.java
@@ -17,120 +17,134 @@ import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
  * 
  */
 public class DocumentParser {
-  /**
-   * PDF file extension.
-   */
-  public static final String EXT_PDF = ".pdf";
-  /**
-   * DOC file extension.
-   */
-  public static final String EXT_DOC = ".doc";
-  /**
-   * Open document text extension.
-   */
-  public static final String EXT_ODT = ".odt";
-  /**
-   * XML extension.
-   */
-  public static final String EXT_XML = ".xml";
-  /**
-   * JPG extension.
-   */
-  public static final String EXT_JPG = ".jpg";
-  /**
-   * TIFF extension.
-   */
-  public static final String EXT_TIFF = ".tiff";
-  /**
-   * PNG extension.
-   */
-  public static final String EXT_PNG = ".png";
-  /**
-   * HTML extension.
-   */
-  public static final String EXT_HTML = ".html";
-  /**
-   * XHTML extension.
-   */
-  public static final String EXT_XHTML = ".xhtml";
-  /**
-   * HTM extension.
-   */
-  public static final String EXT_HTM = ".htm";
-  /**
-   * TXT extension.
-   */
-  public static final String EXT_TXT = ".txt";
+	/**
+	 * PDF file extension.
+	 */
+	public static final String EXT_PDF = ".pdf";
+	/**
+	 * DOC file extension.
+	 */
+	public static final String EXT_DOC = ".doc";
+	/**
+	 * Open document text extension.
+	 */
+	public static final String EXT_ODT = ".odt";
+	/**
+	 * XML extension.
+	 */
+	public static final String EXT_XML = ".xml";
+	/**
+	 * JPG extension.
+	 */
+	public static final String EXT_JPG = ".jpg";
+	/**
+	 * TIFF extension.
+	 */
+	public static final String EXT_TIFF = ".tiff";
+	/**
+	 * PNG extension.
+	 */
+	public static final String EXT_PNG = ".png";
+	/**
+	 * HTML extension.
+	 */
+	public static final String EXT_HTML = ".html";
+	/**
+	 * XHTML extension.
+	 */
+	public static final String EXT_XHTML = ".xhtml";
+	/**
+	 * HTM extension.
+	 */
+	public static final String EXT_HTM = ".htm";
+	/**
+	 * TXT extension.
+	 */
+	public static final String EXT_TXT = ".txt";
+	/**
+	 * DOCX extension
+	 */
+	private static final Object EXT_DOCX = ".docx";
 
-  protected DocumentModelStrategy documentModelBuilder;
+	protected DocumentModelStrategy documentModelBuilder;
 
-  /**
-   * Create a new DocumentParser instance. An instance will offer a
-   * parse()-method which gets a URL to be parsed.
-   */
-  public DocumentParser() {
-    documentModelBuilder = new DocumentModelStrategy();
-  }
+	/**
+	 * Create a new DocumentParser instance. An instance will offer a
+	 * parse()-method which gets a URL to be parsed.
+	 */
+	public DocumentParser() {
+		documentModelBuilder = new DocumentModelStrategy();
+	}
 
-  /**
-   * Parse any kind of document.
-   * 
-   * @throws ApplicationException
-   *           if there's no parser available for the type of resource.
-   * @param url
-   *          - the URL to the document.
-   * @return an {@link IDocument} containing the fulltext and maybe metadata for
-   *         the parsed document.
-   */
-  public IDocument parse(final String url) throws ApplicationException {
-    ResourceParser parser = null;
+	/**
+	 * Parse any kind of document.
+	 * 
+	 * @throws ApplicationException
+	 *             if there's no parser available for the type of resource.
+	 * @param url
+	 *            - the URL to the document.
+	 * @return an {@link IDocument} containing the fulltext and maybe metadata
+	 *         for the parsed document.
+	 */
+	public IDocument parse(final String url) throws ApplicationException {
+		ResourceParser parser = null;
 
-    if (EdocIndexMetadataFetcherTool.isEDocIndex(url)) {
-      parser = EdocParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_PDF)) {
-      parser = PdfParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_DOC)) {
-      parser = DocParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_ODT)) {
-      parser = OdfParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_XML)) {
-      parser = XmlParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_HTM) || getExtension(url).equals(EXT_HTML) || getExtension(url).equals(EXT_XHTML)) {
-      parser = HtmlParserImpl.getInstance();
-    } else if (getExtension(url).equals(EXT_TXT)) {
-      parser = TxtParserImpl.getInstance();
-    }
+		String extension = getExtension(url);
+		if (EdocIndexMetadataFetcherTool.isEDocIndex(url)) {
+			parser = EdocParserImpl.getInstance();
+		} else if (extension.equals(EXT_PDF)) {
+			parser = PdfParserImpl.getInstance();
+		} else if (extension.equals(EXT_DOC)) {
+			parser = DocParserImpl.getInstance();
+		} else if (extension.equals(EXT_ODT)) {
+			parser = OdfParserImpl.getInstance();
+		} else if (extension.equals(EXT_XML)) {
+			parser = XmlParserImpl.getInstance();
+		} else if (extension.equals(EXT_HTM) || extension.equals(EXT_HTML)
+				|| extension.equals(EXT_XHTML)) {
+			parser = HtmlParserImpl.getInstance();
+		} else if (extension.equals(EXT_TXT)) {
+			parser = TxtParserImpl.getInstance();
+		}
 
-    if (parser != null) {
-      final IDocument result = (IDocument) parser.parse("", url);
-      return result;
-    } else {
-      throw new ApplicationException("There's no parser available for this type of resource: " + getExtension(url));
-    }
-  }
+		if (parser != null) {
+			final IDocument result = (IDocument) parser.parse("", url);
+			return result;
+		} else {
+			if (extension.equals(EXT_DOCX)) {
+				throw new ApplicationException(
+						"(Microsoft Windows 2007) .docx format isn't supported by Apache TIKA yet. Check out future releases which use the new Apache POI package which includes docx support.");
+			}
+			throw new ApplicationException(
+					"There's no parser available for this type of resource: "
+							+ extension);
+		}
+	}
 
-  /**
-   * Fetch the URI's extension.
-   */
-  public String getExtension(final String uri) {
-    final int extPos = uri.lastIndexOf(".");
-    final String extension = uri.substring(extPos, uri.length());
+	/**
+	 * Fetch the URI's extension.
+	 */
+	public String getExtension(final String uri) {
+		final int extPos = uri.lastIndexOf(".");
+		final String extension = uri.substring(extPos, uri.length());
 
-    return extension;
-  }
+		return extension;
+	}
 
-  /**
-   * Check if the resource is an image.
-   * 
-   * @param uri
-   *          - the resource's URI.
-   * @return true if the resource is an image.
-   */
-  public boolean isImage(final String uri) {
-    if (getExtension(uri).equals(EXT_JPG) || getExtension(uri).equals(EXT_TIFF) || getExtension(uri).equals(EXT_PNG)) {
-      return true;
-    }
-    return false;
-  }
+	/**
+	 * Check if the resource is an image.
+	 * 
+	 * @param uri
+	 *            - the resource's URI.
+	 * @return true if the resource is an image.
+	 */
+	public boolean isImage(final String uri) {
+		if (getExtension(uri).equals(EXT_JPG)
+				|| getExtension(uri).equals(EXT_TIFF)
+				|| getExtension(uri).equals(EXT_PNG)) {
+			return true;
+		}
+		return false;
+	}
 
 }
