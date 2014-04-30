ea4c3ae438142d690831bbbcc82045fdedf827f6
joey <willenborg@bbaw.de>
2014-04-30 14:49:13 +0200

    neue RDF-Syntax für dcterms:subject

--- src/org/bbaw/wsp/cms/lucene/IndexHandler.java
+++ src/org/bbaw/wsp/cms/lucene/IndexHandler.java
@@ -277,8 +277,8 @@ public class IndexHandler {
       }
       String subjectControlledDetails = mdRecord.getSubjectControlledDetails();
       if (subjectControlledDetails != null) {
-        String namespaceDeclaration = "declare namespace rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"; declare namespace dc=\"http://purl.org/dc/elements/1.1/\"; declare namespace dcterms=\"http://purl.org/dc/terms/\"; ";
-        String dcTermsSubjectsStr = xQueryEvaluator.evaluateAsStringValueJoined(subjectControlledDetails, namespaceDeclaration + "/subjects/dcterms:subject", "###");
+        String namespaceDeclaration = "declare namespace rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"; declare namespace rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"; declare namespace dc=\"http://purl.org/dc/elements/1.1/\"; declare namespace dcterms=\"http://purl.org/dc/terms/\"; ";
+        String dcTermsSubjectsStr = xQueryEvaluator.evaluateAsStringValueJoined(subjectControlledDetails, namespaceDeclaration + "/subjects/dcterms:subject/rdf:Description/rdfs:label", "###");
         String[] dcTermsSubjects = dcTermsSubjectsStr.split("###");
         for (int i=0; i<dcTermsSubjects.length; i++) {
           String s = dcTermsSubjects[i].trim();
