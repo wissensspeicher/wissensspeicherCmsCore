1ff93527ae8bd7060c22f8a10e398dcb32fd9a67
Marco Juergens <juergens@bbaw.de>
2014-09-24 18:50:43 +0200

    zeugs

--- src/org/bbaw/wsp/cms/mdsystem/metadata/mdqueryhandler/detailedsearch/HitGraph.java
+++ src/org/bbaw/wsp/cms/mdsystem/metadata/mdqueryhandler/detailedsearch/HitGraph.java
@@ -6,8 +6,13 @@ package org.bbaw.wsp.cms.mdsystem.metadata.mdqueryhandler.detailedsearch;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
+import org.apache.log4j.Logger;
 import org.bbaw.wsp.cms.mdsystem.metadata.rdfmanager.WspRdfStore;
+
+import com.hp.hpl.jena.rdf.model.RDFNode;
 
 /**
  * @author <a href="mailto:sascha.feldmann@gmx.de">Sascha Feldmann</a>
@@ -18,8 +23,10 @@ public class HitGraph {
 
   private final URL namedGraphUrl;
   private final ArrayList<HitStatement> hitStatements;
+  private final HashMap<String, HashMap<String, RDFNode>> hitStatementsMap;
   private double avgScore;
   private double highestScore;
+  private static final Logger logger = Logger.getLogger(SparqlAdapter.class);
 
   public static final double DEFAULT_SCORE = 0;
 
@@ -32,6 +39,7 @@ public class HitGraph {
   public HitGraph(final URL namedGraphUrl) {
     this.namedGraphUrl = namedGraphUrl;
     hitStatements = new ArrayList<HitStatement>();
+    hitStatementsMap = new HashMap<String, HashMap<String, RDFNode>>();
     avgScore = DEFAULT_SCORE;
     highestScore = DEFAULT_SCORE;
   }
@@ -56,6 +64,19 @@ public class HitGraph {
     return highestScore;
   }
 
+  /**
+   * Add a {@link HitStatement } and subject as key. Should be done by the {@link SparqlAdapter} only.
+   * 
+   * @param statement a {@link HitStatement}
+   * @param subject
+   */
+  public void addStatement(String subject, final HashMap<String, RDFNode> descrMap) {
+    hitStatementsMap.put(subject, descrMap);
+    // calculate scores
+//    calcAvgScore(statement);
+//    calcHighestScore(statement);
+  }
+  
   /**
    * Add a {@link HitStatement}. Should be done by the {@link SparqlAdapter} only.
    * 
@@ -101,7 +122,20 @@ public class HitGraph {
   public Collection<HitStatement> getAllHitStatements() {
     return hitStatements;
   }
+  
+  /**
+   * Return the {@link HitStatement} of this {@link HitGraph}.
+   * 
+   * @return
+   */
+  public HashMap<String, HashMap<String, RDFNode>> getAllHitStatementsAsMap() {
+    return hitStatementsMap;
+  }
 
+  public HashMap<String, RDFNode> getStatementBySubject(String subject){
+   return hitStatementsMap.get(subject);
+  }
+  
   /*
    * (non-Javadoc)
    * 
@@ -109,7 +143,22 @@ public class HitGraph {
    */
   @Override
   public String toString() {
-    return "HitGraph [namedGraphUrl=" + namedGraphUrl + ", hitStatements=" + hitStatements + ", avgScore=" + avgScore + ", highestScore=" + highestScore + "]";
+    logger.info("hitgraph to string");
+    StringBuilder sb = new StringBuilder();
+    sb.append("HitGraph [namedGraphUrl=" + namedGraphUrl + ", hitStatementsMap= \n");
+    for (Entry<String, HashMap<String, RDFNode>> entry : hitStatementsMap.entrySet()) {
+      sb.append("  "+(entry.getKey() + " : \n"));
+      sb.append("\n");
+      HashMap<String, RDFNode> hmRdfnode = entry.getValue();
+      for (Entry<String, RDFNode> entryRdf : hmRdfnode.entrySet()) {
+        sb.append("    "+(entryRdf.getKey() + " : " + entryRdf.getValue()));
+        sb.append("\n");
+      }
+      sb.append("\n");
+    }
+    sb.append(", avgScore=" + avgScore + ", highestScore=" + highestScore + "]");
+    sb.append("\n");
+    return sb.toString();
   }
 
 }
