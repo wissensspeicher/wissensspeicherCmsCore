76e86eeef9f11874f98d6e536078d3e4965e6ef6
joey <willenborg@bbaw.de>
2014-11-04 16:11:58 +0100

    Performance: Einlesen von DBpedia-Labeln aus einer Datei

--- src/org/bbaw/wsp/cms/dochandler/DBpediaSpotlightHandler.java
+++ src/org/bbaw/wsp/cms/dochandler/DBpediaSpotlightHandler.java
@@ -1,11 +1,14 @@
 package org.bbaw.wsp.cms.dochandler;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Scanner;
 
 import net.sf.saxon.s9api.XdmItem;
 import net.sf.saxon.s9api.XdmSequenceIterator;
@@ -16,14 +19,17 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.bbaw.wsp.cms.document.Annotation;
 import org.bbaw.wsp.cms.document.DBpediaResource;
+import org.bbaw.wsp.cms.general.Constants;
 
 import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
 import de.mpg.mpiwg.berlin.mpdl.util.StringUtils;
 import de.mpg.mpiwg.berlin.mpdl.xml.xquery.XQueryEvaluator;
 
 public class DBpediaSpotlightHandler {
+  private static Logger LOGGER = Logger.getLogger(DBpediaSpotlightHandler.class);
   private static DBpediaSpotlightHandler instance;
   private static int SOCKET_TIMEOUT = 200 * 1000;
   // private static String HOSTNAME = "localhost";
@@ -32,6 +38,7 @@ public class DBpediaSpotlightHandler {
   private static String SERVICE_PATH = "spotlight/rest";
   private HttpClient httpClient; 
   private XQueryEvaluator xQueryEvaluator;
+  private Hashtable<String, DBpediaResource> dbPediaResources;
   private int counter = 0;
 
   public static DBpediaSpotlightHandler getInstance() throws ApplicationException {
@@ -44,6 +51,7 @@ public class DBpediaSpotlightHandler {
   
   private void init() throws ApplicationException {
     initHttpClient();
+    initDBpediaResources();
   }
   
   private void initHttpClient() {
@@ -56,6 +64,35 @@ public class DBpediaSpotlightHandler {
     xQueryEvaluator = new XQueryEvaluator();
   }
   
+  private void initDBpediaResources() throws ApplicationException {
+    dbPediaResources = new Hashtable<String, DBpediaResource>();
+    initDBpediaResourceLabels();
+    LOGGER.info("DBpediaSpotlightHandler initialized with: " + dbPediaResources.size() + " DBpedia resources");
+  }
+  
+  private void initDBpediaResourceLabels() throws ApplicationException {
+    String dbPediaResourcesDirName = Constants.getInstance().getExternalDocumentsDir() +  "/dbPediaResources";
+    File dbPediaResourceLabelsFile = new File(dbPediaResourcesDirName + "/labels.ttl");
+    try {
+      Scanner s = new Scanner(dbPediaResourceLabelsFile, "utf-8");
+      while (s.hasNextLine()) {
+        String line = s.nextLine();
+        if (! line.startsWith("#")) {
+          String[] triple = line.split("> ");
+          String uri = triple[0].substring(1);
+          int to = triple[2].lastIndexOf("\"");
+          String label = triple[2].substring(1, to);
+          DBpediaResource dbPediaResource = new DBpediaResource();
+          dbPediaResource.setUri(uri);
+          dbPediaResource.setName(label);
+          dbPediaResources.put(uri, dbPediaResource);
+        }
+      }
+    } catch (IOException e) {
+      throw new ApplicationException(e);
+    }
+  }
+
   public Annotation annotate(String docId, String text, String confidence, int count) throws ApplicationException {
     counter++;
     if (counter == 100) {
@@ -90,6 +127,14 @@ public class DBpediaSpotlightHandler {
           String frequencyStr = xQueryEvaluator.evaluateAsString(spotlightAnnotationXmlStr, "count(//Resource[@URI = '" + uriStrEscaped + "'])");
           DBpediaResource r = new DBpediaResource();
           r.setUri(uriStr);
+          DBpediaResource dbPediaResource = dbPediaResources.get(uriStr);
+          if (dbPediaResource != null) {
+            r.setName(dbPediaResource.getName());
+          } else {
+            int begin = uriStr.lastIndexOf("resource/") + 9;
+            String name = uriStr.substring(begin);
+            r.setName(name);
+          }
           if (supportStr != null && ! supportStr.isEmpty())
             r.setSupport(new Integer(supportStr));
           if (similarityStr != null && ! similarityStr.isEmpty())
@@ -122,6 +167,12 @@ public class DBpediaSpotlightHandler {
       for (int i=0; i<count; i++) {
         mainResources.add(resources.get(i));
       }
+      // TODO
+      // fetch all names of mainResources by SparQL: e.g. these 2: 
+      // SELECT ?name WHERE {
+      //   {<http://de.dbpedia.org/resource/Karl_Marx> foaf:name ?name .} union
+      //   {<http://de.dbpedia.org/resource/Friedrich_Engels> foaf:name ?name .} 
+      // }
       annotation.setResources(mainResources);
     }
     return annotation;
