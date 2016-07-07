fe30f61399d01f98e1a2a53579e2bf981648c153
joey <willenborg@bbaw.de>
2016-07-07 16:08:14 +0200

    toDate robuster gemacht

---
+++ src/org/bbaw/wsp/cms/util/Util.java
@@ -0,0 +1,50 @@
+package org.bbaw.wsp.cms.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import de.mpg.mpiwg.berlin.mpdl.exception.ApplicationException;
+
+public class Util {
+
+  public String toYearStr(String inputStr) {
+    String retYearStr = inputStr.trim();
+    int index = inputStr.indexOf("-");
+    if (index > 0) {
+      retYearStr = inputStr.substring(0, index);
+      retYearStr = retYearStr.trim();
+    }
+    return retYearStr;
+  }
+
+  public Date toDate(String xsDateStr) throws ApplicationException {
+    Date retDate = null;
+    if (xsDateStr == null || xsDateStr.equals("0") || xsDateStr.equals("00") || xsDateStr.equals("000") || xsDateStr.equals("0000"))
+      return null;
+    try {
+      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+      retDate = dateFormat.parse(xsDateStr);
+    } catch (ParseException e) {
+      throw new ApplicationException(e);
+    }
+    return retDate;
+  }
+
+  public String toXsDate(Date date) {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    String xsDateStr = dateFormat.format(date);
+    return xsDateStr;
+  }
+  
+  public Double getSecondWithMillisecondsBetween(Date begin, Date end) {
+    long beginMS = begin.getTime();
+    long endMS = end.getTime();
+    long elapsedSeconds = (endMS - beginMS) / 1000;
+    long elapsedMilliSecondsAfterSeconds1 = (endMS - beginMS) - (elapsedSeconds * 1000);
+    Double seconds = new Double(elapsedSeconds + "." + elapsedMilliSecondsAfterSeconds1); 
+    return seconds;
+  }
+ 
+}
