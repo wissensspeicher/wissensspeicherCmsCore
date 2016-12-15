17d50d3cdc0f3addd9acc2ccba67019ccabab008
joey <willenborg@bbaw.de>
2016-12-15 10:17:27 +0100

    eintragung von exludes in der Vatercollection

--- src/org/bbaw/wsp/cms/collections/Project.java
+++ src/org/bbaw/wsp/cms/collections/Project.java
@@ -258,7 +258,6 @@ public class Project {
         ProjectCollection parentCollection = getCollection(collParentRdfId);
         parentCollection.addSubCollection(collection);
         // subcollections with crawlDB's: add exclude
-        /*
         ArrayList<Database> parentCrawlDBs = parentCollection.getDatabasesByType("crawl");
         ArrayList<Database> subcollectionCrawlDBs = collection.getDatabasesByType("crawl");
         if (parentCrawlDBs != null && subcollectionCrawlDBs != null) {
@@ -271,7 +270,6 @@ public class Project {
             parentCrawlDB.addExclude(excludeStr);
           }
         }
-        */
       }
     }
   }
